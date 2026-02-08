81d273d2ca43bad6ecd5c857c8f01cb27702ef52
smohammadali613-web <smohammadali613@gmail.com>
2026-02-09 01:37:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function login() {
-  // Temporary frontend login (no Cognito)
-  window.location.href = "dashboard.html";
-}
+const API_URL = "https://API_URL_HERE/prod/process";
 
-function selectOption(type) {
-  document.getElementById("output").innerHTML =
-    "You selected: " + type;
+function callService(action) {
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerText = "Processing...";
+
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      action: action,
+      input: "test input from user"
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      outputDiv.innerText = data.output;
+    })
+    .catch(error => {
+      outputDiv.innerText = "Error calling API";
+      console.error(error);
+    });
 }
