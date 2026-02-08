284470b9bba847d30eddb2d590c460ee27f50903
smohammadali613-web <smohammadali613@gmail.com>
2026-02-08 23:38:48 +0530

    Add Cognito configuration and login/logout functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Frontend loaded");
+
+/* ===== Cognito Configuration ===== */
+const cognitoConfig = {
+  domain: "ap-south-1mwd0fdgvv.auth.ap-south-1.amazoncognito.com",
+  clientId: "25go7uI0prdtu9johhriei22fr",
+  redirectSignIn: "https://main.d1kqze7yks7kh5.amplifyapp.com/dashboard.html",
+  redirectSignOut: "https://main.d1kqze7yks7kh5.amplifyapp.com",
+  responseType: "code"
+};
+
+/* ===== Login Function ===== */
+function login() {
+  const loginUrl =
+    "https://" + cognitoConfig.domain +
+    "/login" +
+    "?client_id=" + cognitoConfig.clientId +
+    "&response_type=" + cognitoConfig.responseType +
+    "&scope=email+openid+profile" +
+    "&redirect_uri=" + encodeURIComponent(cognitoConfig.redirectSignIn);
+
+  window.location.href = loginUrl;
+}
+
+/* ===== Logout Function (optional) ===== */
+function logout() {
+  const logoutUrl =
+    "https://" + cognitoConfig.domain +
+    "/logout" +
+    "?client_id=" + cognitoConfig.clientId +
+    "&logout_uri=" + encodeURIComponent(cognitoConfig.redirectSignOut);
+
+  window.location.href = logoutUrl;
+}
