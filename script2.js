7a5e6fc50d6a8b6f63c2b764735786feafadcfc8
smohammadali613-web <smohammadali613@gmail.com>
2026-02-09 20:30:09 +0530

    Add functions for command generation and checking

---
+++ script2.js
@@ -0,0 +1,11 @@
+function commandGenerator() {
+    document.getElementById("output").innerText = "Command Generator selected!";
+}
+
+function websiteChecker() {
+    document.getElementById("output").innerText = "Website Checker selected!";
+}
+
+function digitalMessage() {
+    document.getElementById("output").innerText = "Digital Message selected!";
+}
