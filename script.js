481d586924fc98b75e82210d04947a2f598b500c
smohammadali613-web <smohammadali613@gmail.com>
2026-02-09 21:37:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-async function commandGenerator() {
-    const output = document.getElementById("output");
-    output.innerText = "Loading...";
+async function callBedrock() {
+  document.getElementById("output").innerText = "Calling backend...";
 
-    try {
-        const response = await fetch("https://b6j2gj5ta4k4ffiezheiszgtyu0nlpji.lambda-url.ap-south-1.on.aws/");
-        const data = await response.json();
+  try {
+    const response = await fetch("PASTE_LAMBDA_FUNCTION_URL_HERE");
 
-        output.innerText =
-            "Service: " + data.service + "\n\n" +
-            data.message + "\n\n" +
-            "Example Commands:\n- " +
-            data.example_commands.join("\n- ");
-    } catch (error) {
-        output.innerText = "Backend error!";
-        console.error("ERROR:", error);
-    }
+    const data = await response.json();
+
+    document.getElementById("output").innerText = data.message;
+
+  } catch (error) {
+    document.getElementById("output").innerText = "Backend error";
+    console.error(error);
+  }
 }
