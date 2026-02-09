76a240b5d55208cf5ce6742aa56a3f7dae63e5e1
smohammadali613-web <smohammadali613@gmail.com>
2026-02-09 21:03:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-async function commandGenerator() {
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerText = "Loading...";
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    try {
-        const response = await fetch("https://b6j2gj5ta4k4ffiezheiszgtyu0nlpji.lambda-url.ap-south-1.on.aws/");
-        const data = await response.json();
-
-        outputDiv.innerText =
-            "Service: " + data.service + "\n\n" +
-            data.message + "\n\n" +
-            "Commands:\n- " + data.example_commands.join("\n- ");
-    } catch (error) {
-        outputDiv.innerText = "Error connecting to backend!";
-        console.error(error);
+    if (username === "admin" && password === "admin123") {
+        window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("loginMessage").innerText =
+            "Invalid username or password";
     }
+}
+function commandGenerator() {
+    document.getElementById("output").innerText =
+        "Command Generator selected";
+}
+
+function websiteChecker() {
+    document.getElementById("output").innerText =
+        "Website Checker selected";
+}
+
+function digitalMessage() {
+    document.getElementById("output").innerText =
+        "Digital Message selected";
 }
